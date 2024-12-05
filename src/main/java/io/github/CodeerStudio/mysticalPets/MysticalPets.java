bdb6ea5a00f274cedfadc0e624f7ffe03a52ab23
Thomas Froud <thomasfroud10@gmail.com>
2024-12-05 15:09:24 +0000

    Basic 1.21.1 jar file creation setup

--- src/main/java/io/github/CodeerStudio/mysticalPets/MysticalPets.java
+++ src/main/java/io/github/CodeerStudio/mysticalPets/MysticalPets.java
@@ -1,17 +1,18 @@
 package io.github.CodeerStudio.mysticalPets;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MysticalPets extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getLogger().info("MysticalPets Enabled");
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("MysticalPets Disabled");
     }
 }
