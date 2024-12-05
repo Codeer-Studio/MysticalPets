f41aa1feaec3beb95c067498c97a8f44d6ccabf7
Thomas Froud <thomasfroud10@gmail.com>
2024-12-05 12:35:53 +0000

    Initial project creation

---
+++ src/main/java/io/github/CodeerStudio/mysticalPets/MysticalPets.java
@@ -0,0 +1,17 @@
+package io.github.CodeerStudio.mysticalPets;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MysticalPets extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
