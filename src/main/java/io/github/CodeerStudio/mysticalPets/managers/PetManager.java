e868143f6aec36a9743231039d57c345e692a4c8
Thomas Froud <thomasfroud10@gmail.com>
2024-12-05 15:35:59 +0000

    Update PetManager.java
    The summonPet method now creates a armor stand to the left of the player

--- src/main/java/io/github/CodeerStudio/mysticalPets/managers/PetManager.java
+++ src/main/java/io/github/CodeerStudio/mysticalPets/managers/PetManager.java
@@ -1,10 +1,32 @@
 package io.github.CodeerStudio.mysticalPets.managers;
 
+import org.bukkit.Location;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 public class PetManager {
 
     public boolean summonPet(Player player, String petName) {
+
+        Location playerLocation = player.getLocation();
+
+        // Calculate the location to the left of the player
+        Vector direction = playerLocation.getDirection();
+        Vector leftDirection = direction.clone().rotateAroundY(Math.toRadians(90)); // Rotate 90 degrees to the left
+        Location spawnLocation = playerLocation.add(leftDirection.multiply(1.5)); // Offset 1.5 blocks to the left
+
+        // Spawn an armor stand at the calculated location
+        ArmorStand pet = (ArmorStand) player.getWorld().spawnEntity(spawnLocation, EntityType.ARMOR_STAND);
+
+        // Configure the armor stand
+        pet.setCustomName(petName);
+        pet.setCustomNameVisible(true);
+        pet.setGravity(false);
+        pet.setInvisible(true);
+        pet.setSmall(true);
+
         return true;
     }
 }
